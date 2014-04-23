f637821643dc1aa02ad57991fac4870b274dc400
Douglas Silvio Correa <douglas.silvio@gmail.com>
2014-04-22 23:26:14 -0300

    Added some UTs

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -2,21 +2,32 @@
 
 describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('rpsAppApp'));
+    // load the controller's module
+    beforeEach(module('rpsAppApp'));
 
-  var MainCtrl,
-    scope;
+    var MainCtrl,
+        scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        MainCtrl = $controller('MainCtrl', {
+            $scope: scope
+        });
+    }));
+
+    it('should attach a list of possible results to the scope', function () {
+        expect(scope.result_calc.length).toBe(3);
     });
-  }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+    it('should attach a list of ui options to the scope', function () {
+        expect(scope.ui_options.length).toBe(3);
+    });
+
+    it('should show result instead of Play! on the button after user click it', function () {
+        expect(scope.result).toBe('Play!');
+        scope.choose(1);
+        scope.play();
+        expect(scope.result).not.toBe('Play!');
+    });
 });
